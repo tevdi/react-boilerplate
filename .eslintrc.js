9784f5f20c290c1180d9fc5a98a077787da980f0
Jordi <jordi.sanchez@cinnamon.agency>
2020-10-30 10:38:42 +0100

    Added Font Awesome & .eslintrc.js, updated .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = { 
+"extends": "standard",
+"rules": {
+        "indent": "off",
+        "no-tabs": 0,
+	"space-before-blocks": 0,
+	"no-trailing-spaces": 0,
+        "eqeqeq": "off",
+        "space-before-function-paren": 0
+}
+};
+
